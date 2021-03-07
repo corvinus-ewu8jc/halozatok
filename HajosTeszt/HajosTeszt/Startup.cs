8cf7ffd829ecc671797911325603047008b98508
Júlia Südi <julia.sudi@stud.uni-corvinus.hu>
2021-03-07 22:12:05 +0100

    Cv2

--- HajosTeszt/HajosTeszt/Startup.cs
+++ HajosTeszt/HajosTeszt/Startup.cs
@@ -29,7 +29,7 @@ namespace HajosTeszt
 
             DefaultFilesOptions options = new DefaultFilesOptions();
             options.DefaultFileNames.Clear();
-            options.DefaultFileNames.Add("cv.html");
+            options.DefaultFileNames.Add("cv2.html");
             app.UseDefaultFiles(options);
             app.UseRouting();
             app.UseStaticFiles();
